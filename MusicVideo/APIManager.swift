a85c5580c2367d9440ea4a96a6ff2ad73e93f847
hhrvoic <hhrvoic@foi.hr>
2016-03-14 16:37:26 +0100

    completed serialisation from NSData to JSON

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -21,17 +21,37 @@ class APIManager {
         
             let task = session.dataTaskWithURL(url) {
                     (data,response,error) -> Void in
-                        dispatch_async(dispatch_get_main_queue()) { //return to main thread code
                             if error != nil{
+                                 dispatch_async(dispatch_get_main_queue()) { //return to main thread code
                                 completion(result:(error!.localizedDescription) )
+                                }
                             }
                             else { //success
-                                completion (result:"NSURLSession successful")
-                                print(data)
+                                do {
+                                    //convert nsdata to JSON and cast it to dictionary, .allow fragments - top level object is not NSdict/NSarray
+                                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject]
+                                        {
+                                            
+                                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                                            //we are currently in global (background) thread queue, setting the high priority means we will faster get out of it - this background job will have high priority compared to other background jobs
+                                            dispatch_async(dispatch_get_global_queue(priority,0)) {
+                                                //high priority (bg) code = dispatching to main
+                                                dispatch_async(dispatch_get_main_queue()) {
+                                                    completion(result: "JSON serialization succesfull")
+                                                }
+                                            }
+                                        }
+                                }
+                                catch {
+                                    dispatch_async(dispatch_get_main_queue()) {
+                                        completion(result: "Error in JSON serialization")
+                                    }
+ 
+                                }
+                                
                             }
                     }
-                
-                }
+        
             task.resume() //task always start in suspended state, you have to force start manually
         
         }
---
+++ MusicVideo/NSDataToJSONSerializator.swift
@@ -0,0 +1,13 @@
+//
+//  NSDataToJSONSerializator.swift
+//  MusicVideo
+//
+//  Created by hrvoje on 12/03/16.
+//  Copyright © 2016 hrvoje. All rights reserved.
+//
+
+import Foundation
+
+class NSDataToJSONSerializator {
+    public 
+}
