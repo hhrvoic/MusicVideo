31fbc1859223a4a8a2240edea9c22dc3dfe1512d
hhrvoic <hhrvoic@foi.hr>
2016-05-14 18:25:47 +0200

    fetching image from background thread and storing for later

--- MusicVideo/MusicVideoTableViewCell.swift
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -18,16 +18,42 @@ class MusicVideoTableViewCell: UITableViewCell {
     var video : MusicVideo? {
         didSet {
              updateCell()
+            
         }
     }
-
+    
+    func getImageData(video:MusicVideo, imageView:UIImageView){
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0)){
+        
+        let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+        var image : UIImage?
+            if let bytes = data {
+                  video.vImageData = bytes
+                  image = UIImage(data:bytes)
+            }
+            dispatch_async(dispatch_get_main_queue()){
+                imageView.image = image
+                
+            }
+        }
+       
+        
+    }
     
     
     
     func updateCell() {
        rankLabel.text = ("\(video!.vRank)")
        titleLabel.text = video!.vName
-        musicVideoImage.image = UIImage(named: "imageNotAvailable")
+        if let imgData = video!.vImageData { //optional binding
+            musicVideoImage.image = UIImage (data: imgData)
+            print("getting image from previously stored nsdata...")
+        }
+        else {
+            musicVideoImage.image = UIImage(named: "imageNotAvailable")
+            print("getting image from url for the first time/backgorund thread")
+            getImageData(video!, imageView: musicVideoImage)
+        }
     }
 
     }
